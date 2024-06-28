594d7eaeb97fe342001b6762de2b1cde90aa385f
frm <frm@tutao.de>
2024-06-28 14:01:19 +0200

    better understood RSS; added interactive welcome message button using framework and dark mode switch using vanilla JavaScript; finished tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://frm-learning-astro.netlify.app/"
+  site: "https://frm-learning-astro.netlify.app/",
+  integrations: [preact()]
 });
